f81083d7828bc8d1e3a0fc49f374e6b050dbc99a
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2021-03-01 20:09:50 +0800

    Fix config

--- src/main/kotlin/io/hoarfroster/Tag.kt
+++ src/main/kotlin/io/hoarfroster/Tag.kt
@@ -2,6 +2,6 @@ package io.hoarfroster
 
 class Tag internal constructor(var name: String) {
     override fun toString(): String {
-        return "{name: \"$name\"}"
+        return "{\"name\": \"$name\"}"
     }
 }
